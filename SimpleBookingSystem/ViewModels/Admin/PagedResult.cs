77e17d2a12692b371ce9a48e96bba9b4787a5154
nyanlinhtetdev <nlinhtet.dev22@gmail.com>
2026-05-15 11:04:27 +0800

    feat: create a pagination helper

---
+++ SimpleBookingSystem/ViewModels/Admin/PagedResult.cs
@@ -0,0 +1,13 @@
+﻿namespace SimpleBookingSystem.ViewModels.Admin;
+
+public class PagedResult<T>
+{
+    public List<T> Items { get; set; } = new();
+    public int CurrentPage { get; set; }
+    public int TotalPages { get; set; }
+    public int TotalCount { get; set; }
+    public int PageSize { get; set; }
+
+    public bool HasPreviousPage => CurrentPage > 1;
+    public bool HasNextPage => CurrentPage < TotalPages;
+}
